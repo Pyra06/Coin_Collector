9b1211d5b6f6cd346c4180b23dd63029d1534d5b
Pyra06 <aniruddhasharma867@gmail.com>
2022-07-17 11:15:36 +0530

    Coin Collector

---
+++ CollectingCoin/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody rb;
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI vicText;
+    public float speed;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            countText.text = "Count - " + count.ToString();
+            if (count >= 11) 
+            {
+                vicText.gameObject.SetActive(true);
+            }
+        }
+    }
+}
